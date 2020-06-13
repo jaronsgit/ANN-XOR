9503374c42a8a544521f7bd3118f7dafb0439fbe
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-06-13 11:38:56 +0200

    Rename Perceptron.h's output method and add private members
    Output method renamed to predict for understanding purposes.
    numberOfInputs, learningRate, epochThreshold and weights private members
    added to Perceptron.h

--- include/Perceptron.h
+++ include/Perceptron.h
@@ -1,8 +1,12 @@
-#ifndef NODE_H
-#define NODE_H
+#ifndef Perceptron_H
+#define Perceptron_H
 
 #include <vector>
 #include <utility>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 namespace CHNJAR003
 {
@@ -10,16 +14,17 @@ namespace CHNJAR003
     class Perceptron
     {
     private:
+        int numberOfInputs;
         float learningRate; //Real value between 0 and 1
-        float threshold;    //Number of allowable epochs
-        //float bias;
+        //float threshold;
+        float epochThreshold;       //Number of allowable epochs
         std::vector<float> weights; //W0 will effectively be used as a bias
 
     public:
-        Perceptron(const int numberOfInputs, const float learningRate, const int threshold);
-        int output(const std::vector<float> &inputVector);                       //Perceptron output function -> outputs either a 1 or a zero
+        Perceptron(const int numberOfInputs, const float learningRate, const float bias, const int epochThreshold);
+        int predict(const std::vector<float> &inputVector);                      //Perceptron output function -> outputs either a 1 or a zero
         void learn(std::vector<std::pair<std::vector<float>, int>> trainingSet); //Training set is a vector of pairs: first is the input vector, second is the desired ouput
-        ~Perceptron();
+        friend std::ostream &operator<<(std::ostream &os, const Perceptron &p);  //Overloaded output stream operator
     };
 
 } // namespace CHNJAR003
