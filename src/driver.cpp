71969fa6717cdb93d7ed797418236be8f5e9655a
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-06-13 14:32:16 +0200

    Train AND, OR and NAND gate in driver program
    Added code to driver.cpp to train the AND, OR and NAND gate with the training examples
    and test their output predictions for the respective input vectors by printing them to
    standard output.
    
    A learningRate of 0.01 and an epochThreshold of 100 was selected.

--- src/driver.cpp
+++ src/driver.cpp
@@ -46,6 +46,47 @@ std::vector<std::pair<std::vector<float>, int>> constructTrainingSet(const std::
 
 int main(int argc, char *argv[])
 {
+    auto ANDtrainingSet = constructTrainingSet("trainingExamples/AND.txt");
+    auto ORtrainingSet = constructTrainingSet("trainingExamples/OR.txt");
+    auto NANDtrainingSet = constructTrainingSet("trainingExamples/NAND.txt");
+    /*for (auto trainingExample : trainingSet)
+    {
+        for (auto exampleVectorElement : trainingExample.first)
+        {
+            std::cout << exampleVectorElement << " ";
+        }
+        std::cout << ": " << trainingExample.second << std::endl;
+    }*/
 
+    CHNJAR003::Perceptron ANDgate(2, 0.01, 0, 100);
+    ANDgate.learn(ANDtrainingSet);
+    std::cout << "AND predictions:" << std::endl;
+    std::cout << "{0, 0} -> " << ANDgate.predict({0, 0}) << std::endl;
+    std::cout << "{0, 1} -> " << ANDgate.predict({0, 1}) << std::endl;
+    std::cout << "{1, 0} -> " << ANDgate.predict({1, 0}) << std::endl;
+    std::cout << "{1, 1} -> " << ANDgate.predict({1, 1}) << std::endl;
+    std::cout << "________________\n";
+
+    CHNJAR003::Perceptron ORgate(2, 0.01, 0, 100);
+    ORgate.learn(ORtrainingSet);
+    std::cout << "OR predictions:" << std::endl;
+    std::cout << "{0, 0} -> " << ORgate.predict({0, 0}) << std::endl;
+    std::cout << "{0, 1} -> " << ORgate.predict({0, 1}) << std::endl;
+    std::cout << "{1, 0} -> " << ORgate.predict({1, 0}) << std::endl;
+    std::cout << "{1, 1} -> " << ORgate.predict({1, 1}) << std::endl;
+    std::cout << "________________\n";
+
+    CHNJAR003::Perceptron NANDgate(2, 0.01, 0, 100);
+    NANDgate.learn(NANDtrainingSet);
+    std::cout << "NAND predictions:" << std::endl;
+    std::cout << "{0, 0} -> " << NANDgate.predict({0, 0}) << std::endl;
+    std::cout << "{0, 1} -> " << NANDgate.predict({0, 1}) << std::endl;
+    std::cout << "{1, 0} -> " << NANDgate.predict({1, 0}) << std::endl;
+    std::cout << "{1, 1} -> " << NANDgate.predict({1, 1}) << std::endl;
+    std::cout << "________________\n";
+
+    std::cout << ANDgate;
+    std::cout << ORgate;
+    std::cout << NANDgate;
     return 0;
 }
