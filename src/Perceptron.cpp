acf66ba7d0e0f00cc9a2c1e0dd01da85a52706b5
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-06-13 11:45:12 +0200

    Define Perceptron parameterised constructor
    Parameterised constructor method defintion written in Perceptron.cpp

--- src/Perceptron.cpp
+++ src/Perceptron.cpp
@@ -1,12 +1,16 @@
 #include "../include/Perceptron.h"
 #include <vector>
 #include <utility>
+#include <algorithm>
+#include <numeric>
 
 namespace CHNJAR003
 {
 
-    Perceptron::Perceptron(const int numberOfInputs, const float learningRate, const int threshold) :
+    Perceptron::Perceptron(const int numberOfInputs, const float learningRate, const float bias, const int epochThreshold) : numberOfInputs(numberOfInputs), learningRate(learningRate), epochThreshold(epochThreshold)
     {
+        weights = std::vector<float>(numberOfInputs + 1, 0.0);
+        weights[0] = bias;
     }
 
 } // namespace CHNJAR003
