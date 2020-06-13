a3d934cd02383a7022c801e62f66f2d08a56f2f6
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-06-13 15:40:47 +0200

    Create Network.h
    Network class created and yet to be fully defined.
    Class will construct a fully connected feed-forward network
    consisting of Node objects.

---
+++ include/Network.h
@@ -0,0 +1,25 @@
+#ifndef Network_H
+#define Network_H
+
+#include <vector>
+#include <utility>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "../include/Node.h"
+
+namespace CHNJAR003
+{
+    class Network
+    {
+    private:
+        std::vector<int> topology;
+        /* data */
+    public:
+        Network(/* args */);
+    };
+
+} // namespace CHNJAR003
+
+#endif
