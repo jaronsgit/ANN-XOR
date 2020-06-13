92cf0de1bc870b4144ce3b3f426f12bc50c2cc41
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-06-13 12:44:40 +0200

    Create driver.cpp Driver created to interpret the command line parameters and call the necessary class methods to run the program

---
+++ src/driver.cpp
@@ -0,0 +1,13 @@
+#include "../include/Perceptron.h"
+#include "../include/XOR.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <sstream>
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
