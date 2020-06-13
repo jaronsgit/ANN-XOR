a98f8ebf577b1d0bc3221e90be423bf49905c1ab
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-06-13 16:56:47 +0200

    Implement Node parameterised constructor
    Node parameterised constructor implemented in Node.cpp

--- src/Node.cpp
+++ src/Node.cpp
@@ -6,9 +6,13 @@
 
 namespace CHNJAR003
 {
-
     Node::Node(const int numInputs, const std::vector<float> &weights) : numberOfInputs(numInputs), weights(weights)
     {
+    }
+
+    float Node::output(const std::vector<float> &inputConnections)
+    {
+        return std::inner_product(weights.begin() + 1, weights.end(), inputConnections.begin(), weights[0]);
     }
 
 } // namespace CHNJAR003
