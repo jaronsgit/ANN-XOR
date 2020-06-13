3ad991fca9c58f8d1d5930b10fac13fc455b320e
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-06-13 14:24:15 +0200

    Create constructTrainingSet method in driver.cpp
    Defined and implemented constructTrainingSet method, which accepts a file name,
    reads the file, processes the training examples and returns them in a vector of
    pairs needed by the Perceptron class to train the perceptron.

--- src/driver.cpp
+++ src/driver.cpp
@@ -6,6 +6,44 @@
 #include <utility>
 #include <sstream>
 
+std::vector<std::pair<std::vector<float>, int>> constructTrainingSet(const std::string &trainingExamplesFile)
+{
+    std::ifstream inputFile(trainingExamplesFile);
+    std::vector<std::pair<std::vector<float>, int>> trainingSet;
+
+    if (inputFile.is_open())
+    {
+        std::string line;
+
+        while (getline(inputFile, line))
+        {
+            std::istringstream ss(line);
+            std::string token;
+
+            std::vector<std::string> tokens;
+
+            while (getline(ss, token, ' '))
+            {
+                tokens.push_back(token);
+            }
+
+            std::vector<float> exampleVector;
+            int exampleTarget;
+
+            for (int i = 0; i < tokens.size() - 1; ++i)
+            {
+                exampleVector.push_back(std::stof(tokens[i]));
+            }
+            exampleTarget = std::stoi(tokens[tokens.size() - 1]);
+
+            std::pair<std::vector<float>, int> tempPair(exampleVector, exampleTarget);
+
+            trainingSet.push_back(tempPair);
+        }
+    }
+    return trainingSet;
+}
+
 int main(int argc, char *argv[])
 {
 
